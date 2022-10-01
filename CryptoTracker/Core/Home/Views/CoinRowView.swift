d1641a28775377c3d471cf311c5ffa65c297739c
Sajjad <sajad.abedi@veriff.net>
2022-10-01 16:24:55 +0300

    Added coins image to the home page

--- CryptoTracker/Core/Home/Views/CoinRowView.swift
+++ CryptoTracker/Core/Home/Views/CoinRowView.swift
@@ -43,7 +43,7 @@ extension CoinRowView {
                 .font(.caption.monospaced())
                 .foregroundColor(.secondary)
                 .frame(minWidth: 30)
-            Circle()
+            CoinImageView(coin: coin)
                 .frame(width: 30, height: 30)
             Text(coin.symbol.uppercased())
                 .font(.headline.monospaced())
